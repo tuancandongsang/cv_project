b74d98992797e8a83f20f893e1cc38ff7f2b8fe0
tuancan <tuacandongsang@gmail.com>
2023-02-03 00:57:57 +0700

    2.2 dropdown

---
+++ frontend/src/layout/component/Header/component/Dropdown/Dropdown-have-img.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="components-dropdown-demo-placement">
+    <a-dropdown placement="bottomLeft">
+      <span>{{ content }}</span>
+      <template #overlay>
+        <div class="dropdown-container">
+          <div class="dropdown-container-left">
+            <a-menu>
+              <a-menu-item class="alo">
+                <div class="alo2">Tim viec</div>
+              </a-menu-item>
+              <a-menu-item class="alo">
+                <div class="alo2">Tim viec</div>
+              </a-menu-item>
+              <a-menu-item class="alo">
+                <div class="alo2">Tim viec</div>
+              </a-menu-item>
+            </a-menu>
+          </div>
+          <div class="dropdown-container-right"></div>
+        </div>
+      </template>
+    </a-dropdown>
+  </div>
+</template>
+<script>
+import './dropdown.scss';
+export default {
+  props: {
+    content: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
