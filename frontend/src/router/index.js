556283556e89697383513c0b0f38464e38842212
tuancan <tuacandongsang@gmail.com>
2023-02-11 23:31:06 +0700

    aaa

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import NotFound from '../page/PageNotFound/NotFoundPage.vue';
-import Login from '../page/login/login.vue';
+import Login from '../page/login/Login.vue';
+import login_tuyendung from '../page/login-tuyendung/Login-tuyendung.vue';
 import Home from '../page/home/home.vue';
 import Layout from '../layout/layout.vue';
 // import { getJwtToken } from '../utils/helpers';
@@ -9,6 +10,11 @@ export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/login', component: Login, name: 'login' },
+    {
+      path: '/login-tuyendung',
+      component: login_tuyendung,
+      name: 'login_tuyendung',
+    },
     { path: '/sign-up', component: Login, name: 'sign-up' },
     {
       path: '/',
